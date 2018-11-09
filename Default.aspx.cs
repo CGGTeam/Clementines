a27ebd676e50ce5f9ec1c4ed55a4603e5c1d4f0d
von3007 <von3007@live.ca>
2018-11-08 22:23:51 -0500

    multipages

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -5,56 +5,99 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default :  System.Web.UI.Page
+public partial class _Default : System.Web.UI.Page
 {
-    private librairie lib = new librairie();
-    private List<String> films = new List<String>();
+    protected int nbElementsParPage = 10;
+    protected int noPage = 1;
 
+    private librairie lib = new librairie();
+    private List<Film> films = new List<Film>();
+
+    protected class Film {
+        public string nom  {get; private set; }
+        public string vignette { get; private set; }
+        public string personne { get; private set; }
+
+    public Film(string nom, string vignette, string personne)
+        {
+            this.nom = nom;
+            this.vignette = vignette;
+            this.personne = personne;
+        }
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
-        films.Add("Film1");
-        films.Add("Film2");
-        films.Add("Film3");
-        films.Add("Film4");
-        films.Add("Film5");
-        films.Add("Film6");
-        films.Add("Film7");
-        films.Add("Film8");
-        films.Add("Film9");
-        films.Add("Film10");
+        string img = "Static/images/flavicon.png";
+        initialiserNoPage();
+
+        for (int i = 1; i<= 20; i++)
+        {
+            films.Add(new Film("Film" + i, img, "Personne"+i));
+        }
 
         Panel row = lib.divDYN(phDynamique, "row", "row");
         Panel col1 = lib.divDYN(row, "col1", "col-sm-6");
-        for (int i = 0; i < films.Count/2; i++)
-        {
-            afficherFilm(films[i], col1);
-        }
         Panel col2 = lib.divDYN(row, "col2", "col-sm-6");
-        for (int i = films.Count / 2; i < films.Count; i++)
+        for (int i = (noPage * nbElementsParPage) - (nbElementsParPage-1); i <= nbElementsParPage* noPage; i++)
         {
-            afficherFilm(films[i], col2);
+            if(i % 2 != 0)
+                afficherFilm(films[i-1], col1);
+            else
+                afficherFilm(films[i-1], col2);
         }
-        afficherPager(phDynamique);
-    }
 
-    protected void afficherFilm(String film, Control container)
+        Panel row2 = lib.divDYN(phDynamique, "row2", "row");
+        afficherPager(row2);
+    }
+    protected void initialiserNoPage()
     {
-        Panel div = lib.divDYN(container, film, "panel panel-warning");
-        Panel header = lib.divDYN(div, "header" + film, "panel-heading");
-        Panel content = lib.divDYN(div, "content" + film, "panel-body");
-        Label lblHeader = lib.lblDYN(header, "txtHeader" + film, film);
-        Label lblContent = lib.lblDYN(content, "txtContent" + film, film);
+        int n;
+        if (Request.QueryString["Page"] == null || !int.TryParse(Request.QueryString["Page"], out n))
+        {
+            noPage = 1;
+        }
+        else
+        {
+            noPage = n;
+        }
     }
 
-    protected void afficherPager(Control container)
+    protected void afficherFilm(Film film, Control container)
     {
-        /*
-         *Next and Previous Control 
-         */
-
+        Panel div = lib.divDYN(container, film.nom, "panel panel-warning");
+        Panel header = lib.divDYN(div, "header" + film.nom, "panel-heading");
+        Panel content = lib.divDYN(div, "content" + film.nom, "panel-body");
+        Label lblHeader = lib.lblDYN(header, "lbl" + film.nom, film.nom);
+        Image img = lib.imgDYN(content, "img" + film.nom, film.vignette, ".img-rounded col-sm-2");
+        Label lblContent = lib.lblDYN(content, "lblPersonne" + film.nom, film.personne);
     }
 
+    protected void afficherPager(Control control)
+    {
+        LiteralControl pager = new LiteralControl();
+        decimal nbPages = Math.Floor((decimal)films.Count / (decimal)nbElementsParPage);
 
+        int previous = noPage - 1;
+        string strClass = previous <= 0 ? "page-item disabled" : "page-item";
+        string strDebut =  "<nav aria - label = 'Page navigation example' >" +
+                                "<ul class='pagination justify-content-center'>" +
+                                    "<li class='" + strClass + "'><a class='page-link' href='?Page=" + previous+"'>Previous</a></li>";
 
+        pager.Text += strDebut;
+        for(int i = 1; i <= nbPages; i++)
+        {
+            strClass = noPage == i ? "page-item active" : "page-item";
+            string strMillieu = "<li class='"+ strClass + "'><a class='page - link' href='?Page="+ i+"'>"+i+"</a></li>";
+            pager.Text += strMillieu;
+        }
 
+        int next = noPage +1;
+        strClass = next >= nbPages ? "page-item disabled" : "page-item";
+        string strFin = "<li class='" + strClass + "'><a class='page-link' href='?Page=" + previous + "'>Next</a></li>" +
+                                "</ul>" +
+                            "</nav>";
+        pager.Text += strFin;
+
+        control.Controls.Add(pager);
+    }
 }
--- controles-utilisateur/NomFilm.ascx
+++ controles-utilisateur/NomFilm.ascx
@@ -1,51 +1,34 @@
 ﻿<%@ Control Language="C#" %>
 <script runat="server">
-        public String Text                       { set { tb.Text = value; }
-            get { return tb.Text; } }
-        public TextBox Controle                  { get { return tb; } }
-        public RequiredFieldValidator Present    { get { return ValidatorPresent; } }
-        public RegularExpressionValidator Format { get { return ValidatorFormat; } }
-        public string CssClass { set { tb.CssClass = value; } }
-        public string placeholder { set { tb.Attributes.Add("placeholder", value); } }
+    public String Text                       { set { film1.Text = value; }
+        get { return film1.Text; } }
+    public TextBox Controle                  { get { return film1; } }
+    public RequiredFieldValidator Present    { get { return ValidatorPresent; } }
+    public RegularExpressionValidator Format { get { return ValidatorFormat; } }
+    public string CssClass { set { film1.CssClass = value; } }
+    public string placeholder { set { film1.Attributes.Add("placeholder", value); } }
+    static int i = 0;
 
-        protected void ajouterChamp(Object sender, EventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("in");
-
-            TextBox tb = new TextBox
-            {
-                MaxLength = 25,
-                ID = "2",
-                CssClass = "form-control"
-            };
-            moreTb.Controls.Add(tb);
-
-            
-        }
+   
 
 </script>
 
-<asp:TextBox ID="tb" runat="server"
+<asp:TextBox ID="film1" runat="server"
    MaxLength="25"
     placeholder="Doit être présent"/>
 
-<asp:Panel runat="server" ID="moreTb">
-
-</asp:Panel>
-
-<asp:LinkButton  ID="lus" runat="server"
-    onclick="ajouterChamp" AutoPostBack=false >
+<div ID="moreTextboxs"></div>
+    
+<button  onclick = "CreateTxt();return false;" class="btn btn-light">
     <span class="glyphicon glyphicon-plus"></span>
-</asp:LinkButton>
-
-
+</button>
 <asp:RequiredFieldValidator ID="ValidatorPresent" runat="server"
-   ControlToValidate="tb"
+   ControlToValidate="film1"
    EnableClientScript="false"
    Display="None" />
 
 <asp:RegularExpressionValidator ID="ValidatorFormat" runat="server"
-   ControlToValidate="tb"
+   ControlToValidate="film1"
    ValidationExpression="^[a-zA-ZÀ-ÖØ-öø-ÿ]*([,'\- ]{1}[a-zA-ZÀ-ÖØ-öø-ÿ]+)*[a-zA-ZÀ-ÖØ-öø-ÿ]*$"
-   EnableClientScript="false"
+   EnableClientScript="false"                                                  
    Display="None" />
