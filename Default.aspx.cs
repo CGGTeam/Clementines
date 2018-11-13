742340157d8a16b86370f3ad51d5bb4d6bd2dd0b
von3007 <von3007@live.ca>
2018-11-12 21:22:37 -0500

    search almost functionnal

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,7 @@ public partial class _Default : System.Web.UI.Page
 {
     private int nbElementsParPage = 10;
     private int noPage = 1;
+    private string filtre;
 
     private List<Film> filmsOG = new List<Film>();
     private List<Film> films = new List<Film>();
@@ -30,25 +31,41 @@ public partial class _Default : System.Web.UI.Page
             this.personne = personne;
         }
     }
-    public void FilterList(object sender, EventArgs e)
+    public void FilterList()
     {
-        
-        System.Diagnostics.Debug.WriteLine("filter");
-        string filtre = "1";
+        InitialiserSearch();
+
+        bool rechercheParTitre = cbTitre.Checked;
+        bool rechercheParPersonne = cbPersonne.Checked;
+
         films.Clear();
-        films = filmsOG.Where(film => film.nom.Contains(filtre)).ToList();
+        if(rechercheParTitre && rechercheParPersonne)
+            films = filmsOG.Where(film => film.nom.ToLower().Contains(filtre.ToLower()) || film.personne.ToLower().Contains(filtre.ToLower())).ToList();
+        else if(rechercheParPersonne)
+            films = filmsOG.Where(film =>film.personne.ToLower().Contains(filtre.ToLower())).ToList();
+        else if(rechercheParTitre)
+            films = filmsOG.Where(film => film.nom.ToLower().Contains(filtre.ToLower())).ToList();
+        else
+            films = filmsOG.ToList();
+
         AfficherLesFilms(col1, col2, row2);
+
+    }
+    public void UpdateFiltre(object sender, EventArgs e)
+    {
+        string strFiltre = tbRecherche.Text;
+        
+        Response.Redirect("~/Default.aspx?Page=1&Filtre="+ strFiltre, false);
     }
 
     private void Page_Load(object sender, EventArgs e)
     {
-        System.Diagnostics.Debug.WriteLine("load");
+        InitialiserCheckBoxState();
+        InitialiserSearch();
+        InitialiserNoPage();
 
-        //tbRecherche.Attributes.Add("onChange", "FilterList();");
 
         string img = "Static/images/flavicon.png";
-        initialiserNoPage();
-
         for (int i = 1; i<= 32; i++)
         {
             filmsOG.Add(new Film("Film" + i, img, "Personne"+i));
@@ -61,7 +78,13 @@ public partial class _Default : System.Web.UI.Page
 
         row2 = librairie.divDYN(phDynamique, "row2", "row");
         AfficherLesFilms(col1, col2, row2);
-        
+
+        FilterList();
+
+    }
+    private void Page_LoadComplete(object sender, EventArgs e)
+    {
+        tbRecherche.Text = filtre;
     }
     private void AfficherLesFilms(Control col1, Control col2, Control row2)
     {
@@ -72,13 +95,13 @@ public partial class _Default : System.Web.UI.Page
         for (int i = (noPage * nbElementsParPage) - (nbElementsParPage - 1); i <= nbElementsParPage * noPage && i <= films.Count(); i++)
         {
             if (i % 2 != 0)
-                afficherFilm(films[i - 1], col1);
+                AfficherFilm(films[i - 1], col1);
             else
-                afficherFilm(films[i - 1], col2);
+                AfficherFilm(films[i - 1], col2);
         }
-        afficherPager(row2);
+        AfficherPager(row2);
     }
-    private void initialiserNoPage()
+    private void InitialiserNoPage()
     {
         int n;
         if (Request.QueryString["Page"] == null || !int.TryParse(Request.QueryString["Page"], out n))
@@ -90,8 +113,38 @@ public partial class _Default : System.Web.UI.Page
             noPage = n;
         }
     }
-
-    private void afficherFilm(Film film, Control container)
+    private void InitialiserSearch()
+    {
+        if (Request.QueryString["Filtre"] == null)
+        {
+            System.Diagnostics.Debug.WriteLine("vide");
+            filtre = "";
+        }
+        else
+        {
+            filtre = Request.QueryString["Filtre"];
+        }
+    }
+    public void Check(object sender, EventArgs e)
+    {
+        CheckBox cb = (CheckBox)sender;
+        System.Diagnostics.Debug.WriteLine(cb.ID);
+        if (cb == cbPersonne)
+        {
+            hfPersonne.Value = cbPersonne.Checked ? "true" : "false";
+        }
+        else if(cb == cbTitre)
+        {
+            hfTitre.Value = cbTitre.Checked ? "true" : "false";
+        }
+        System.Diagnostics.Debug.WriteLine(hfPersonne.Value);
+    }
+    private void InitialiserCheckBoxState()
+    {
+        cbPersonne.Checked = Convert.ToBoolean(hfPersonne.Value);
+        cbTitre.Checked = Convert.ToBoolean(hfTitre.Value);
+    }
+    private void AfficherFilm(Film film, Control container)
     {
         Panel div = librairie.divDYN(container, film.nom, "panel panel-warning");
         Panel header = librairie.divDYN(div, "header" + film.nom, "panel-heading");
@@ -105,7 +158,7 @@ public partial class _Default : System.Web.UI.Page
             ID = "btnEmail"+film.personne,
             CssClass = "btn btn-primary",           
         };
-        email.Click += new EventHandler(envoyerUnCourriel);
+        email.Click += new EventHandler(EnvoyerUnCourriel);
         content.Controls.Add(email);
 
         Panel glyphEmail = new Panel()
@@ -116,10 +169,12 @@ public partial class _Default : System.Web.UI.Page
         email.Controls.Add(glyphEmail);
     }
 
-    private void afficherPager(Control control)
+    private void AfficherPager(Control control)
     {
         LiteralControl pager = new LiteralControl();
         decimal nbPages = Math.Ceiling((decimal)films.Count / (decimal)nbElementsParPage);
+
+        string strFiltre = filtre == "" ? "" : "&Filtre="+filtre;
 
         int previous = noPage - 1;
         string strClass = previous <= 0 ? "page-item disabled" : "page-item";
@@ -127,13 +182,13 @@ public partial class _Default : System.Web.UI.Page
 
         string strDebut =  "<nav aria - label = 'Page navigation example' >" +
                                 "<ul class='pagination justify-content-center'>" +
-                                    "<li class='" + strClass + "'><a class='page-link' href='?Page=" + previous+"'> Previous</a></li>";
+                                    "<li class='" + strClass + "'><a class='page-link' href='?Page=" + previous+ strFiltre + "'> Previous</a></li>";
 
         pager.Text += strDebut;
         for(int i = 1; i <= nbPages; i++)
         {
             strClass = noPage == i ? "page-item active" : "page-item";
-            string strMillieu = "<li class='"+ strClass + "'><a class='page - link' href='?Page="+ i+"'>"+i+"</a></li>";
+            string strMillieu = "<li class='"+ strClass + "'><a class='page - link' href='?Page="+ i+ strFiltre + "'>" +i+"</a></li>";
             pager.Text += strMillieu;
         }
 
@@ -141,14 +196,14 @@ public partial class _Default : System.Web.UI.Page
         strClass = next >= nbPages + 1 ? "page-item disabled" : "page-item";
         next = next >= nbPages + 1 ? noPage : next;
 
-        string strFin = "<li class='" + strClass + "'><a class='page-link' href='?Page=" + next + "'>Next</a></li>" +
+        string strFin = "<li class='" + strClass + "'><a class='page-link' href='?Page=" + next + strFiltre+ "'>Next</a></li>" +
                                 "</ul>" +
                             "</nav>";
         pager.Text += strFin;
 
         control.Controls.Add(pager);
     }
-    private void envoyerUnCourriel(object sender, EventArgs e)
+    private void EnvoyerUnCourriel(object sender, EventArgs e)
     {
         Response.Redirect("~/Pages/Courriel.aspx");
     }
